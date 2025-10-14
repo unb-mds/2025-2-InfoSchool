b3f3f54bea15fbc0b129deea243e40679b4c98ce
macedoPedro20 <pedrinhodelcastilo@gmail.com>
2025-10-14 15:17:57 -0300

    features(server.js): adicionado as rotas

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,36 @@
+import Fastify from "fastify";
 
+const app = Fastify();
+
+app.get("/pagina-inicial", async () => {
+  return { message: "Rota inicial funcionando!" };
+});
+
+app.get("/mapa-brasil", async () => {
+  return { message: "Rota do mapa funcionando!" };
+});
+
+app.get("/ranking", async () => {
+  return { message: "Rota do ranking funcionando!" };
+});
+
+app.get("/rag", async () => {
+  return { message: "Rota do rag funcionando!" };
+});
+
+app.get("/mapa-estado", async () => {
+  return { message: "Rota do mapa dos estados funcionando!" };
+});
+
+app.get("/mapa-municipio", async () => {
+  return { message: "Rota do mapa dos municípios funcionando!" };
+});
+
+app.get("/dashboard", async () => {
+  return { message: "Rota do dashboard funcionando!" };
+});
+
+app.listen({ port: 3000 }, (err) => {
+  if (err) throw err;
+  console.log(`Pagina inicial rodando em http://localhost:3000/pagina-inicial`);
+});
